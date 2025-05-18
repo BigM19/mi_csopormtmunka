afd43e8ebc2933516ba974855eea64b5af7aee89
BigM19 <nmatedan@gmail.com>
2025-05-18 20:57:02 +0200

    További felesleges oszlopok kiszűrése

--- tiktok.py
+++ tiktok.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 
+### Adatfeldolgozás ###
+
 #Adatok beolvasása
 data = pd.read_csv("tiktok_dataset.csv", sep=",")
 print(data.head)
@@ -10,7 +12,8 @@ print("Adatmezők és típusaik:")
 print(data.dtypes)
 
 #Felesleges oszlopok kiszűrése
-data.drop(columns=["#", "video_id", "video_transcription_text"], inplace=True)
+data.drop(columns=["#", "video_id", "video_transcription_text",
+                   "verified_status", "author_ban_status"], inplace=True)
 
 #Hiányzó adatok kezelése
 missing_values = data.isnull().sum()
@@ -22,22 +25,19 @@ data=data.dropna()
 #Duplikált sorok ellenőrzése és eltávolítása
 data = data.drop_duplicates()
 
+### Adatok vizsgálata ###
+
 #Alapstatisztikák kiszámítása: videóhossz, nézettség, like
 duration_stats = data["video_duration_sec"].agg(['mean', 'median', 'std'])
 print("Videóhossz statisztikák:")
 print(duration_stats)
 
-#Repository cloned
-# Adat betöltése
-df = pd.read_csv("tiktok_dataset.csv", sep=",")
-
 # Like/View arány számítása
-df['like_view_ratio'] = df['video_like_count'] / df['video_view_count']
+data['like_view_ratio'] = data['video_like_count'] / data['video_view_count']
 
 # Claim és opinion videók szűrése
-claim_videos = df[df['claim_status'] == 'claim']
-opinion_videos = df[df['claim_status'] == 'opinion']
-
+claim_videos = data[data['claim_status'] == 'claim']
+opinion_videos = data[data['claim_status'] == 'opinion']
 
 # Számosság
 claim_count = len(claim_videos)
@@ -64,21 +64,26 @@ print("Claim statisztikák:", claim_stats)
 print("Opinion statisztikák:", opinion_stats)
 
 #Megosztó videók keresése
-global_mean = df['like_view_ratio'].mean()
-global_std = df['like_view_ratio'].std()
+global_mean = data['like_view_ratio'].mean()
+global_std = data['like_view_ratio'].std()
 threshold = global_mean - global_std
-high_view_threshold = df['video_view_count'].quantile(0.75)
+high_view_threshold = data['video_view_count'].quantile(0.75)
 
-divisive_videos = df[
-    (df['video_view_count'] >= high_view_threshold) &
-    (df['like_view_ratio'] < threshold)
+divisive_videos = data[
+    (data['video_view_count'] >= high_view_threshold) &
+    (data['like_view_ratio'] < threshold)
 ]
 
 divisive_count = len(divisive_videos)
-total_count = len(df)
-divisive_sample = divisive_videos[['video_id', 'video_view_count', 'video_like_count', 'like_view_ratio']].head(5)
+total_count = len(data)
+divisive_sample = divisive_videos[['video_view_count', 'video_like_count', 'like_view_ratio']].head(5)
 
 #kiírás
 print("Megosztó videók száma:", divisive_count)
 print("Összes videó száma:", total_count)
 print("Példák megosztó videókra:\n", divisive_sample)
+
+### Modellezés ###
+
+
+
